b0353bbc08619c6d7342b1a18bc0dd0538b06bfd
vishnumatluru <121228637+vishnumatluru@users.noreply.github.com>
2024-02-01 19:56:16 +0530

    Update variables.tf

--- Jenkins-CICD/variables.tf
+++ Jenkins-CICD/variables.tf
@@ -1,6 +1,6 @@
 variable "aws_region" {
   description = "The AWS region to create things in."
-  default     = "ap-southeast-2"
+  default     = "eu-west-3"
 }
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
@@ -12,7 +12,7 @@ variable "instance_type" {
 }
 variable "ami_id" {
   description = "AMI for Ubuntu Ec2 instance"
-  default     = "ami-0df4b2961410d4cff"
+  default     = "ami-01d21b7be69801c2f"
 }
 variable "bucket_name" {
   description = "The name of the S3 bucket to create"
