3c1d98351d8e4552d054e475f666138e26dc8e7f
vishnumatluru <121228637+vishnumatluru@users.noreply.github.com>
2024-02-01 20:01:08 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "ishnumatluru143" # Replace with your actual S3 bucket name
+    bucket = "vishnumatluru143" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
     region = "eu-west-3"
   }
