6d160150ed1e5960ceab5cb85285c48d95afc8f9
vishnumatluru <121228637+vishnumatluru@users.noreply.github.com>
2024-02-01 19:59:53 +0530

    Update variables.tf

--- Jenkins-CICD/variables.tf
+++ Jenkins-CICD/variables.tf
@@ -4,7 +4,7 @@ variable "aws_region" {
 }
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     = "purplehaze"
+  default     = "ansible"
 }
 variable "instance_type" {
   description = "instance type for ec2"
@@ -17,7 +17,7 @@ variable "ami_id" {
 variable "bucket_name" {
   description = "The name of the S3 bucket to create"
   type        = string
-  default     = "purplehaze777"
+  default     = "vishnumatluru143"
 }
 
 variable "dynamodb_table" {
