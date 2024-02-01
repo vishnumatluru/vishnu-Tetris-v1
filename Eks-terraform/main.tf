2e34d4b4e9a63fd1aa7d0db9a6f7073e40d6ac36
vishnumatluru <121228637+vishnumatluru@users.noreply.github.com>
2024-02-01 23:07:50 +0530

    Update main.tf

--- Eks-terraform/main.tf
+++ Eks-terraform/main.tf
@@ -34,7 +34,7 @@ data "aws_subnets" "public" {
 }
 #cluster provision
 resource "aws_eks_cluster" "example" {
-  name     = "EKS_CLOUD"
+  name     = "CLOUD_EKS"
   role_arn = aws_iam_role.example.arn
 
   vpc_config {
@@ -81,7 +81,7 @@ resource "aws_iam_role_policy_attachment" "example-AmazonEC2ContainerRegistryRea
 #create node group
 resource "aws_eks_node_group" "example" {
   cluster_name    = aws_eks_cluster.example.name
-  node_group_name = "Node-cloud"
+  node_group_name = "cloud-Node"
   node_role_arn   = aws_iam_role.example1.arn
   subnet_ids      = data.aws_subnets.public.ids
 
